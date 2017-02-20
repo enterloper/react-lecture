da9c0e17e1d5cf3bd80fb593de72130238483ba3
richardjboothe <richardjboothe@gmail.com>
2017-02-20 11:27:10 -0600

    add action

--- client_side/src/components/HOC/RequireAuth.js
+++ client_side/src/components/HOC/RequireAuth.js
@@ -6,10 +6,26 @@ export default function(ComposedComponent) {
     static contextTypes = {
       router: React.PropTypes.object
     }
+
+    componentWillMount() {
+      if(!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if(!nextProps.authenticated) {
+        this.context.router.push('/')
+      }
+    }
+
     render(){
       console.log(this.context)
-      return <ComposedComponent {...props} />
+      return <ComposedComponent {...this.props} />
     }
+  }
+  function mapStateToProps(state) {
+    return {authenticated: state.authenticated}
   }
   return connect(mapStateToProps)(Authentication);
 }
