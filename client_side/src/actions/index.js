ab1d2b0bbaa29452e29c5df698720310fb605d03
richardjboothe <richardjboothe@gmail.com>
2017-02-20 08:09:57 -0600

    add axios and call to jsonplaceholder

--- client_side/src/actions/index.js
+++ client_side/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import {
   CHANGE_AUTH,
   FETCH_USERS,
@@ -11,12 +13,24 @@ export function authenticate(isLoggedIn) {
 }
 
 export function fetchUsers() {
+  //the axios request returns a promise, so we have to get the data we want from the request and place it inside of our action
+  //Once that action flows into the reducers. To prevent a race condition where on the one hand we want immediate access to the data in request
+  //but on the other hand we've got a request that takes some x amount of time.
+  const request = axios.get('https://jsonplaceholder.typicode.com/users');
+  console.log('Request:', request);
+  request.then(x=>{
+    console.log(x);
+  })
+  // return {
+  //   type: FETCH_USERS,
+  //   payload: [
+  //     { name: 'Han' },
+  //     { name: 'Yoda' },
+  //     { name: 'Luke' },
+  //   ]
+  // }
   return {
     type: FETCH_USERS,
-    payload: [
-      { name: 'Han' },
-      { name: 'Yoda' },
-      { name: 'Luke' },
-    ]
+    payload: request,
   }
 }
